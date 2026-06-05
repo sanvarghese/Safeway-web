7060b9dba29891051d851dadb96d91b97c0758f5
Bazithz <sanjose0023@gmail.com>
2026-06-05 10:01:19 +0530

    refactor: remove unwanted comments in script files

--- js/theia-sticky-sidebar.js
+++ js/theia-sticky-sidebar.js
@@ -1,12 +1,4 @@
-/*!
- * Theia Sticky Sidebar v1.7.0
- * https://github.com/WeCodePixels/theia-sticky-sidebar
- *
- * Glues your website's sidebars, making them permanently visible while scrolling.
- *
- * Copyright 2013-2016 WeCodePixels and other contributors
- * Released under the MIT license
- */
+
 (function ($) {
     "use strict";
     $.fn.theiaStickySidebar = function (options) {
